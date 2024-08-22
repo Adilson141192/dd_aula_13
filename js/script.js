b7b245115b8debfa2a8c9e430a0bd65bfa4d2994
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-22 14:08:46 -0300

    082224 aula1304

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ function adicionarItem(item){
     valorTotal[item] =  Number.parseFloat(valorProduto[item]) * qtd[item];
     quantidade.innerHTML = qtd[item];
     total.innerHTML = valorTotal[item].toFixed(2);
-
+    valorCompra()
+    
 }
 
 
@@ -23,5 +24,21 @@ function removerItem(item){
         quantidade.innerHTML = qtd[item];
         valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
         total.innerHTML = valorTotal[item].toFixed(2);
+        valorCompra()
     }
+
+}
+
+
+function valorCompra(){
+
+    var valorTotalCompra = document.getElementById('valorTotalCompra');
+    var valor = 0;
+
+    for(var i = 0; i < valorTotal.length; i++){
+        valor += valorTotal[i];
+
+    }
+
+    valorTotalCompra.innerHTML = valor.toFixed(2);
 }
